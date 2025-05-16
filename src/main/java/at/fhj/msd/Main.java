d340af9b1fa8185a18c889378f3b0bdb4a055ca6
Maryam Taeid <maryam.taeid@edu.fh-joanneum.at>
2025-05-16 12:31:42 +0200

    fix some bugs

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,5 +1,5 @@
 package at.fhj.msd;
-vpublic class Main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Testing MyStackArray:");
         MyStackArray<Integer> stackArray = new MyStackArray<>();
